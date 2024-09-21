d96102d0058b3e2bef0869f76648d50e6bcd875f
Alhillawii <ayahhillawi9@gmail.com>
2024-09-21 19:22:57 +0300

    massege

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  
+    console.log('Form Submitted: ', formData);
+  };
+
+  return (
+    <div className="signup-form">
+     
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Last Name</label>
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Email Address</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <button type="submit">submit</button>
+      </form>
+
+      {submitted && <p>Form submitted successfully!</p>}
+
+     
+    </div>
+  );
+};
+
+export default SignupForm;
